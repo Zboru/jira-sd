38cde0b9363139ce8226a65c12e06e4864ba72b8
Sebastian Zborowski <sebastian.zborowski@enp.pl>
2022-09-02 12:30:02 +0200

    dodanie svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  plugins: [svelte()],
   build: {
     sourcemap: true,
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'src/main.ts'),
         background: path.resolve(__dirname, 'src/background.ts'),
-        popup: path.resolve(__dirname, 'src/popup.ts'),
+        popup: path.resolve(__dirname, 'src/popup/main.ts'),
       },
       output: {
         sourcemap: 'inline',
         entryFileNames: 'assets/js/[name].js',
+        assetFileNames: '[name].[ext]',
       },
     },
   },
