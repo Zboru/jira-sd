5ab6e02ad2da0cc2bd574adc12f207b324e48443
Sebastian Zborowski <sebastian.zborowski@enp.pl>
2022-09-02 12:43:54 +0200

    svelte headlessui + tailwind

--- src/popup/App.svelte
+++ src/popup/App.svelte
@@ -1,47 +1,11 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
+import Tabs from "./components/Tabs.svelte";
+
 </script>
 
 <main>
-  <div>
-    <b>SUPER</b>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <Tabs/>
 </main>
 
-<style>
-  b {
-    text-decoration: underline  ;
-  }
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+
+<style></style>
